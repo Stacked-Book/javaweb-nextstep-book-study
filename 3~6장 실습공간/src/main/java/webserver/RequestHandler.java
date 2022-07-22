7ee3a17ecaf2ec03ad9e6a7c44b3f197b318386e
keemsora <itsyoursora@gmail.com>
2022-07-22 21:12:09 +0900

    Feat: 추가 로그인 기능
    - 로그인 시 Set-Cookie 값 설정
    - 요청 헤더의 Cookie 값 확인

--- 3~6장 실습공간/src/main/java/webserver/RequestHandler.java
+++ 3~6장 실습공간/src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,9 +59,31 @@ public class RequestHandler extends Thread {
                 Map<String, String> params = httpRequestUtils.parseQueryString(requestBody);
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("User : {}", user);
+                DataBase.addUser(user);
 
                 DataOutputStream dos = new DataOutputStream(out);
                 response302Header(dos);
+            } else if(url.equals("/user/login")) {
+                String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                log.debug("Request Body : {}", requestBody);
+                Map<String, String> params = httpRequestUtils.parseQueryString(requestBody);
+                log.debug("userId : {}, password : {}", params.get("userId"), params.get("password"));
+                User user = DataBase.findUserById(params.get("userId"));
+                if (user == null) {
+                    log.debug("User Not Found");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302Header(dos);
+                }
+                if (params.get("password").equals(user.getPassword())) {
+                    log.debug("login success");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302WithCookie(dos, "logined=true");
+                } else {
+                    log.debug("Password Mismatch");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302WithCookie(dos, "logined=false");
+                }
+
             } else {
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] body = Files.readAllBytes(new File("./webapp", url).toPath());
@@ -106,6 +129,16 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void response302WithCookie(DataOutputStream dos, String cookie) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: /index.html\r\n");
+            dos.writeBytes("Set-Cookie: " + cookie + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
