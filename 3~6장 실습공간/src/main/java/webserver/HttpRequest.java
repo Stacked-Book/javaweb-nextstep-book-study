a262946da99f804de68d54619967227f8075a824
https://github.com/youngreal <dudwls0505@naver.com>
2022-08-05 14:43:02 +0900

    refactor: HttpRequest
    HttpRequest의 일부책임을 RequestLine클래스에 분리하게되면서 리팩토링

--- 3~6장 실습공간/src/main/java/webserver/HttpRequest.java
+++ 3~6장 실습공간/src/main/java/webserver/HttpRequest.java
@@ -1,66 +1,67 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
+import util.RequestLine;
+
 import java.io.*;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpRequest {
-    public static final int HTTP_METHOD_NUMBER = 4;
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
     private final BufferedReader br;
-    private final InputStream testDR;
+    private final InputStream in;
     private final Map<String, String> headerMap = new ConcurrentHashMap<>();
-    private final Map<String, String> parameterMap = new ConcurrentHashMap<>();
 
-    public HttpRequest(InputStream in) {
-        this.testDR = in;
+
+    private Map<String, String> paramsMap = new ConcurrentHashMap<>();
+    private RequestLine requestLine;
+
+    public HttpRequest(InputStream in) throws IOException {
+        this.in = in;
         this.br = new BufferedReader(new InputStreamReader(in));
+        String line = br.readLine();
+
+        this.requestLine = new RequestLine(line);
+
+        String temp = br.readLine();
+        while (!temp.equals("")) {
+            log.debug("line= {}" , line);
+            String[] split = temp.split(" ");
+            split[0] = split[0].replace(":", "");
+            this.headerMap.put(split[0], split[1]);
+            temp = br.readLine();
+        }
+
+        if (getMethod().equals("POST")) {
+            String body = IOUtils.readData(br, Integer.parseInt(headerMap.get("Content-Length")));
+            this.paramsMap = HttpRequestUtils.parseQueryString(body);
+        } else {
+            this.paramsMap = requestLine.getParameter();
+        }
     }
 
-    public String getMethod() throws IOException {
-        String line = this.br.readLine();
-        String[] requestLine = line.split(" ");
-        this.headerMap.put("method", requestLine[0]);
-        this.headerMap.put("path", requestLine[1]);
-        this.headerMap.put("httpVer", requestLine[2]);
-        return this.headerMap.get("method");
+    public String getMethod() {
+        return this.requestLine.getMethod();
     }
 
     public String getPath() throws IOException {
-        String path = this.headerMap.get("path");
-        if (path.contains("?")) {
-            return path.substring(0, path.indexOf("?"));
-        }
-        return path;
+        return this.requestLine.getPath();
     }
 
-    public Object getHeader(String connection) throws IOException {
-        String temp = this.br.readLine();
-        while (!temp.equals("")) {
-            String[] line = temp.split(" ");
-            line[0] = line[0].replace(":", "");
-            this.headerMap.put(line[0], line[1]);
-            temp = br.readLine();
-        }
+    public Object getHeader(String connection){
         return this.headerMap.get(connection);
     }
 
-    public Object getParameter(String userId) throws IOException {
-        String method = this.headerMap.get("method");
-        String path;
+    public Object getParameter(String userId) {
+        return this.paramsMap.get(userId);
+    }
 
-        if (method.equals("GET")) {
-            path = this.headerMap.get("path");
-            path = path.substring(path.indexOf("?") + 1);
-        }
-        else {
-            path = br.readLine();
-        }
 
-        String[] params = path.split("&");
-        for (String param : params) {
-            String[] split = param.split("=");
-            this.parameterMap.put(split[0], split[1]);
-        }
-        return this.parameterMap.get(userId);
+    public Map<String, String> getParamsMap() {
+        return this.paramsMap;
     }
 }
