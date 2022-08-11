89492521c3938c7173adab1d731ec20bb302519f
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-11 19:01:14 +0900

    Feat : RequestBody 생성

---
+++ 3~6장 실습공간/src/main/java/util/request/RequestBody.java
@@ -0,0 +1,13 @@
+package util.request;
+
+public class RequestBody {
+    private final String body;
+
+    public RequestBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+}
---
+++ 3~6장 실습공간/src/test/java/util/request/RequestBodyTest.java
@@ -0,0 +1,36 @@
+package util.request;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class RequestBodyTest {
+
+    /**
+     * 요청 body 정보를 가집니다.
+     */
+    @Test
+    public void getRequestBody() {
+        assertDoesNotThrow(
+            () -> new RequestBody("body")
+        );
+    }
+
+    /**
+     * body 정보를 반홥합니다.
+     */
+    @Test
+    public void RequestBody_getBody() {
+        // given
+        String body = "body";
+
+        // when
+        RequestBody requestBody = new RequestBody(body);
+
+        // then
+        String actual = requestBody.getBody();
+        assertThat(actual).isEqualTo("body");
+    }
+}
