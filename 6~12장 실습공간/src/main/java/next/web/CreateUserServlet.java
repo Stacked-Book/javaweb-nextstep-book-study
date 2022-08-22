e2a2353ed31ea1f49f4a051948a3d577204fbc3e
https://github.com/youngreal <dudwls0505@naver.com>
2022-08-22 15:53:35 +0900

    ch6 init

---
+++ 6~12장 실습공간/src/main/java/next/web/CreateUserServlet.java
@@ -0,0 +1,28 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/create")
+public class CreateUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(CreateUserServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                req.getParameter("email"));
+        log.debug("user : {}", user);
+        DataBase.addUser(user);
+        resp.sendRedirect("/user/list");
+    }
+}
