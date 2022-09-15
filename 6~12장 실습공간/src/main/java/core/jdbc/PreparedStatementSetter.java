32c9024f391021102e41c1046405cfec92b2e0a9
keemsora <itsyoursora@gmail.com>
2022-09-15 20:20:04 +0900

    feat: 8-2 Ajax 구현 추가

--- 6~12장 실습공간/src/main/java/core/jdbc/PreparedStatementSetter.java
+++
@@ -1,9 +0,0 @@
-package core.jdbc;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface PreparedStatementSetter {
-
-    void setValues(PreparedStatement pstmt) throws SQLException;
-}
